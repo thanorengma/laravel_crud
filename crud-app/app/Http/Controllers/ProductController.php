bd351cb45a752d25eef4100ff1f4be79aca410bd
Thano Rengma <thanojishing@gmail.com>
2024-05-16 11:15:38 +0530

    create basic route,controller,view and model

--- crud-app/app/Http/Controllers/ProductController.php
+++ crud-app/app/Http/Controllers/ProductController.php
@@ -7,9 +7,6 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     public function index(){
-        return view('products.index');
-    }
-    public function create(){
-        return view('products.create');
+        return view('product.index');
     }
 }
