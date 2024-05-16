fe8a9fd6441ec9a015529ac286c92405c0dd8f9d
Thano Rengma <thanojishing@gmail.com>
2024-05-16 09:58:52 +0530

    first commit

---
+++ crud-app/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/product',[ProductController::class,'index'])->name('product.index');
+Route::get('/product/create',[ProductController::class,'create'])->name('product.create');
